d5fbfd027ef67e56e83e312d8957c4ce9b96d9a6
Ivan Fuertes <ivan.fuertes@wikitude.com>
2020-01-09 19:25:26 +0100

    AG10 Completed!!!

---
+++ projects/AG10/main.cpp
@@ -0,0 +1,136 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <glm/gtc/matrix_transform.hpp>
+
+#include "engine/camera.hpp"
+#include "engine/geometry/cube.hpp"
+#include "engine/input.hpp"
+#include "engine/shader.hpp"
+#include "engine/texture.hpp"
+#include "engine/window.hpp"
+#include "engine/geometry/sphere.hpp"
+#include "engine/geometry/quad.hpp"
+#include "engine/geometry/teapot.hpp"
+#include "engine/model.hpp"
+
+Camera camera(glm::vec3(0.0f, 0.0f, 3.0f));
+
+glm::vec3 dirLightDirection(-0.2f, -1.0f, -0.3f);
+
+glm::vec3 pointLightPositions[] = {
+    glm::vec3(4.0f, 2.0f, 0.0f),
+    glm::vec3(-4.0f, 2.0f, 0.0f)
+};
+
+glm::vec3 cubePositions[] = {
+    glm::vec3(4.0f, 0.0f, 0.0f),
+    glm::vec3(-4.0f, 0.0f, 0.0f),
+    glm::vec3(0.0f, 0.0f, 4.0f),
+    glm::vec3(0.0f, 0.0f, -4.0f),
+    glm::vec3(4.0f, 0.0f, 4.0f),
+    glm::vec3(4.0f, 0.0f, -4.0f),
+    glm::vec3(-4.0f, 0.0f, 4.0f),
+    glm::vec3(-4.0f, 0.0f, -4.0f),
+};
+
+float lastFrame = 0.0f;
+float lastX, lastY;
+bool firstMouse = true;
+
+void handleInput(float dt) {
+    Input* input = Input::instance();
+
+    if (input->isKeyPressed(GLFW_KEY_W)) {
+        camera.handleKeyboard(Camera::Movement::Forward, dt);
+    }
+    if (input->isKeyPressed(GLFW_KEY_S)) {
+        camera.handleKeyboard(Camera::Movement::Backward, dt);
+    }
+    if (input->isKeyPressed(GLFW_KEY_A)) {
+        camera.handleKeyboard(Camera::Movement::Left, dt);
+    }
+    if (input->isKeyPressed(GLFW_KEY_D)) {
+        camera.handleKeyboard(Camera::Movement::Right, dt);
+    }
+}
+
+void onKeyPress(int key, int action) {
+    if (key == GLFW_KEY_Q && action == GLFW_PRESS) {
+        Window::instance()->setCaptureMode(true);
+    }
+
+    if (key == GLFW_KEY_E && action == GLFW_PRESS) {
+        Window::instance()->setCaptureMode(false);
+    }
+}
+
+void onMouseMoved(float x, float y) {
+    if (firstMouse) {
+        firstMouse = false;
+        lastX = x;
+        lastY = y;
+    }
+
+    const float xoffset = x - lastX;
+    const float yoffset = lastY - y;
+    lastX = x;
+    lastY = y;
+
+    camera.handleMouseMovement(xoffset, yoffset);
+}
+
+void onScrollMoved(float x, float y) {
+    camera.handleMouseScroll(y);
+}
+
+void render(const Model& object, const Shader& s_phong) {
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    glm::mat4 view = camera.getViewMatrix();
+    glm::mat4 proj = glm::perspective(glm::radians(camera.getFOV()), 800.0f / 600.0f, 0.1f, 100.0f);
+
+    s_phong.use();
+    glm::mat4 model = glm::mat4(1.0f);
+    //model = glm::translate(model, glm::vec3(0.0f, -0.5f, 0.0f));
+    //model = glm::rotate(model, glm::radians(-90.0f), glm::vec3(1.0f, 0.0f, 0.0f));
+    model = glm::scale(model, glm::vec3(0.01f, 0.01f, 0.01f));
+    s_phong.set("model", model);
+    s_phong.set("view", view);
+    s_phong.set("proj", proj);
+
+    glm::mat3 normalMat = glm::inverse(glm::transpose(glm::mat3(model)));
+    s_phong.set("normalMat", normalMat);
+
+    object.render(s_phong);
+}
+
+int main(int, char* []) {
+    Window* window = Window::instance();
+
+    glClearColor(0.0f, 0.3f, 0.6f, 1.0f);
+
+    const Shader s_phong("../projects/AG10/simple.vs", "../projects/AG10/simple.fs");
+    const Model object("../assets/models/formula 1/Formula 1 mesh.obj");
+
+    glEnable(GL_CULL_FACE);
+    glCullFace(GL_BACK);
+
+    glEnable(GL_DEPTH_TEST);
+    glDepthFunc(GL_LESS);
+
+    Input::instance()->setKeyPressedCallback(onKeyPress);
+    Input::instance()->setMouseMoveCallback(onMouseMoved);
+    Input::instance()->setScrollMoveCallback(onScrollMoved);
+
+    while (window->alive()) {
+        const float currentFrame = glfwGetTime();
+        const float deltaTime = currentFrame - lastFrame;
+        lastFrame = currentFrame;
+
+        handleInput(deltaTime);
+        render(object, s_phong);
+        window->frame();
+    }
+
+    return 0;
+}
